e952efbfebc4d5a1a86afa3a72f736794ecf27ea
Elmerluis0129 <es21-1354@unphu.edu.do>
2025-05-19 21:22:15 -0400

    Agregando el token

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "68a946fbeacfb549"; }
+/******/ 		__webpack_require__.h = function() { return "ea5a15ec384d8e69"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
