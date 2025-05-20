097c4cb43b32beb6e42aaa8b188d98ff575e659f
Elmerluis0129 <es21-1354@unphu.edu.do>
2025-05-19 23:46:46 -0400

    Token actualizado

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -91,18 +91,6 @@
 /******/ 		};
 /******/ 	}();
 /******/ 	
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	!function() {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = function(module) {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				function() { return module['default']; } :
-/******/ 				function() { return module; };
-/******/ 			__webpack_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	}();
-/******/ 	
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	!function() {
 /******/ 		// define getter functions for harmony exports
@@ -131,7 +119,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ea5a15ec384d8e69"; }
+/******/ 		__webpack_require__.h = function() { return "e3f2a9749ebe5649"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
